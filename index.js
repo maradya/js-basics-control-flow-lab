146c49506cea825f1739cbe2ac2c7dfc08916840
Marady <maradyalexis@gmail.com>
2021-05-17 22:30:11 -0400

    commit tree bad20144b4dc7d652b19fc72cabb1aede7223b31

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+      return ("This one is on me!");
+  } else if  (ride < 2200) {
+     return ("I will gladly take your thirty bucks."); 
+    } else if (ride < 2600) {
+     return ("No can do.");
+   }  
+} 
+function ternaryCheckCity(city) {
   // Write your code here!
+  if (city === "NYC") {
+    return ("Ok, sounds good."); 
+  }else{
+    return ("No go.");
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous") {
+    return ("Thank you so much.");
+} else if (tip === "not as generous") {
+    return ("Thank you.");
+} else {
+    return ("Bye.");
+  }
 }
